465a95b8265c62c336d98956f9e4bb4a6cd1fe98
cokeIII <coke2484@gmail.com>
2019-07-06 15:37:25 +0700

    map point

--- platforms/android/app/build/intermediates/merged_assets/debug/mergeDebugAssets/out/app/map.js
+++ platforms/android/app/build/intermediates/merged_assets/debug/mergeDebugAssets/out/app/map.js
@@ -1,5 +1,16 @@
 var orientation = require('nativescript-orientation');
- 
-exports.pageLoaded = function() {
-    orientation.setOrientation("portrait"); 
+var Observable = require("data/observable");
+
+var pageData = new Observable.fromObject({
+    roadName: "testName"
+})
+exports.pageLoaded = function(args) {
+    page = args.object
+    page.bindingContext = pageData
+    console.log(pageData)
+    orientation.setOrientation("portrait")
+    // var webView = page.getViewById("webViewID")
+    // if(webView.android) { // in IOS android will be undefined
+    // webView.android.getSettings().setBuiltInZoomControls(false);
+    // }
 };
--- platforms/android/app/src/main/assets/app/map.js
+++ platforms/android/app/src/main/assets/app/map.js
@@ -1,5 +1,42 @@
 var orientation = require('nativescript-orientation');
+var Observable = require("data/observable");
+
+var pageData = new Observable.fromObject({
+    roadName: "testName"
+})
+exports.pageLoaded = function(args) {
+    page = args.object
+    page.bindingContext = pageData
+    console.log(pageData)
+    orientation.setOrientation("portrait")
+    var bluetooth = require("nativescript-bluetooth");
  
-exports.pageLoaded = function() {
-    orientation.setOrientation("portrait"); 
+    bluetooth.isBluetoothEnabled().then(
+    function(enabled) {
+        console.log("Enabled? " + enabled);
+    }
+    );
+    bluetooth.enable().then(
+        function(enabled) {
+          // use Bluetooth features if enabled is true 
+          bluetooth.startScanning({
+            serviceUUIDs: [],
+            seconds: 10,
+            onDiscovered: function (peripheral) {
+                console.log("Periperhal found with UUID: " + peripheral.UUID);
+                console.log(peripheral)
+            },
+            skipPermissionCheck: false,
+          }).then(function() {
+            console.log("scanning complete");
+          }, function (err) {
+            console.log("error while scanning: " + err);
+          });
+    
+        }
+      );
+    // var webView = page.getViewById("webViewID")
+    // if(webView.android) { // in IOS android will be undefined
+    // webView.android.getSettings().setBuiltInZoomControls(false);
+    // }
 };

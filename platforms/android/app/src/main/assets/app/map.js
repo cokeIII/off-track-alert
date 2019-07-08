d116e9ce2c0f45d28338d38f9d404752a87aa7d0
cokeIII <coke2484@gmail.com>
2019-07-08 16:09:25 +0700

    gen map

--- platforms/android/app/src/main/assets/app/map.js
+++ platforms/android/app/src/main/assets/app/map.js
@@ -1,42 +1,60 @@
 var orientation = require('nativescript-orientation');
 var Observable = require("data/observable");
+var bluetooth = require("nativescript-bluetooth");
+const appSettings = require("application-settings");
 
+const API_URL = "http://192.168.43.50:3001"
 var pageData = new Observable.fromObject({
-    roadName: "testName"
+    roadName: "testName",
+    map:{}
 })
 exports.pageLoaded = function(args) {
     page = args.object
     page.bindingContext = pageData
-    console.log(pageData)
     orientation.setOrientation("portrait")
-    var bluetooth = require("nativescript-bluetooth");
- 
-    bluetooth.isBluetoothEnabled().then(
-    function(enabled) {
-        console.log("Enabled? " + enabled);
-    }
-    );
+
+    let urlMap = API_URL + '/maps'
+
+    fetch(urlMap).then(r => r.json())
+    .then(jsonData => {
+      appSettings.setString("maps", JSON.stringify(jsonData))
+      pageData.map = JSON.parse(appSettings.getString("maps"))
+      console.log(pageData.map.maps)
+    }).catch(e => {
+      console.log('***fetch error***')
+    })
+    BLE_scan()
     bluetooth.enable().then(
         function(enabled) {
-          // use Bluetooth features if enabled is true 
-          bluetooth.startScanning({
-            serviceUUIDs: [],
-            seconds: 10,
-            onDiscovered: function (peripheral) {
-                console.log("Periperhal found with UUID: " + peripheral.UUID);
-                console.log(peripheral)
-            },
-            skipPermissionCheck: false,
-          }).then(function() {
-            console.log("scanning complete");
-          }, function (err) {
-            console.log("error while scanning: " + err);
-          });
-    
+            setInterval(function(){ 
+        
+            // use Bluetooth features if enabled is true 
+            // BLE_scan()
+            
+            }, 11000)
         }
-      );
-    // var webView = page.getViewById("webViewID")
-    // if(webView.android) { // in IOS android will be undefined
-    // webView.android.getSettings().setBuiltInZoomControls(false);
-    // }
-};
+      )
+}
+
+function BLE_scan(){
+    bluetooth.startScanning({
+        serviceUUIDs: [],
+        seconds: 10,
+        onDiscovered: function (peripheral) {
+            console.log("Periperhal found with UUID: " + peripheral.UUID)
+            genMap(peripheral.UUID)
+            // console.log(peripheral)
+        },
+        skipPermissionCheck: false,
+    }).then(function() {
+        console.log("scanning complete")
+    }, function (err) {
+        console.log("error while scanning: " + err)
+    })
+}
+
+function genMap(UUID){
+    pageData.map.maps.forEach(element => {
+        console.log(element)
+    });
+}   
